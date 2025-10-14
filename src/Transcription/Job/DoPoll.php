f82bf752bf170476df08bbc7c8f790289ec03216
Jim Safley <jimsafley@gmail.com>
2025-10-14 15:41:18 -0400

    Continue dev

--- src/Transcription/Job/DoPoll.php
+++ src/Transcription/Job/DoPoll.php
@@ -10,7 +10,6 @@ class DoPoll extends AbstractTranscriptionJob
     {
         $entityManager = $this->get('Omeka\EntityManager');
         $apiManager = $this->get('Omeka\ApiManager');
-        $fileStore = $this->get('Omeka\File\Store');
         $logger = $this->get('Omeka\Logger');
 
         $transcriptionIds = $apiManager->search(
@@ -25,6 +24,51 @@ class DoPoll extends AbstractTranscriptionJob
                 $transcription = $entityManager
                     ->getRepository(ServicesTranscriptionTranscription::class)
                     ->find($transcriptionId);
+                $page = $transcription->getPage();
+
+                $logger->notice(sprintf(
+                    'Polling transcription for media ID %s page ID %s',
+                    $page->getMedia()->getId(),
+                    $page->getId()
+                ));
+
+                if (null === $transcription->getJobId()) {
+                    $logger->notice('Not possible when transcription not initiated');
+                    continue;
+                }
+                if (in_array($transcription->getJobState(), ['completed', 'failed', 'cancelled'])) {
+                    $logger->notice(sprintf(
+                        'Already polled with status "%s"',
+                        $transcription->getJobState()
+                    ));
+                    continue;
+                }
+
+                // Poll Mino for the transcription.
+                $client = $this->get('Omeka\HttpClient')
+                    ->setMethod('GET')
+                    ->setUri(sprintf('https://mino.tropy.org/transcription/%s', $transcription->getJobId()));
+                $headers = $client->getRequest()->getHeaders();
+                $headers->addHeaders([
+                    'Authorization' => sprintf('Bearer %s', $this->getProject()->getAccessToken()),
+                ]);
+                $response = $client->send();
+                if (!$response->isSuccess()) {
+                    throw new \Exception(sprintf(
+                        'Poll failed with status "%s": %s',
+                        $response->getStatusCode(),
+                        $response->getContent()
+                    ));
+                }
+                $content = json_decode($response->getContent(), true);
+
+                $transcription->setJobState($content['state']);
+                $transcription->setText($content['output']['text']);
+                $transcription->setData($content['output']['alto']);
+                $logger->notice(sprintf(
+                    'Poll done with status "%s"',
+                    $content['state']
+                ));
             }
             $entityManager->flush();
         }
